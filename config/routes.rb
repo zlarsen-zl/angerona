6b1f739783498e903d23e122e5d50cf77de99100
Zoe Larsen <zlarsen.zl@gmail.co>
2023-06-08 14:04:40 -0600

    shares route, controller, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :passwords
+  resources :passwords do
+    resources :shares, only: [:create, :new, :destroy]
+  end
 
   # Defines the root path route ("/")
   root "passwords#index"
